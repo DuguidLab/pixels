dc5af7c6c2ce3e94df54d3988e34026452676d2a
mcol <mcol@posteo.net>
2020-04-02 15:41:14 +0100

    create high level data processing organisation

--- pixels/experiment.py
+++ pixels/experiment.py
@@ -1,50 +1,49 @@
+"""
+This module provides an Experiment class which serves as the main interface to process
+data and run subsequent analyses.
+"""
 
 
-import datetime
-import json
 import os
+
+from pixels.session import Session, list_sessions
 
 
 class Experiment:
     def __init__(self, mouse_ids, data_dir, meta_dir):
-        if not isinstance(mouse_ids, (List, Tuple, Set)):
+        if not isinstance(mouse_ids, (list, tuple, set)):
             mouse_ids = Tuple(mouse_ids)
 
         self.mouse_ids = mouse_ids
-        self.sessions = []
-        self.sessions_by_mouse_id = {}
-        self.read_sessions(data_dir, meta_dir)
+        self.data_dir = data_dir
+        self.meta_dir = meta_dir
+        self.sessions = list_sessions(mouse_ids, data_dir, meta_dir)
 
-    def read_sessions(self, data_dir, meta_dir):
-        data_dir = os.path.expanduser(data_dir)
-        meta_dir = os.path.expanduser(meta_dir)
-        available_sessions = os.listdir(data_dir)
+    def extract_spikes(self, resample=True):
+        """
+        Extract the spikes from raw spike data for all sessions.
+        """
+        for session in self.sessions:
+            session.extract_spikes(resample=resample)
 
-        for mouse in mouse_ids:
-            mouse_sessions = []
-            for session in available_sessions:
-                if mouse in session:
-                    mouse_sessions.append(session)
-
-            if mouse_sessions:
-                with open(os.path.join(metadata, mouse + '.json'), 'r') as fd:
-                    mouse_meta = json.load(fd)
-                session_dates = [
-                    datetime.datetime.strptime(s[0:6], '%y%m%d') for s in mouse_sessions
-                ]
-                for session in mouse_meta:
-                    meta_date = datetime.datetime.strptime(session['date'], '%Y-%m-%d')
-                    for index, ses_date in enumerate(session_dates):
-                        if ses_date == meta_date:
-                            if session.get('exclude', False):
-                                continue
-                            mouse_sessions[index] = pixels.Session(metadata=session)
-            else:
-                print(f'Found no sessions for: {mouse}')
-
-            self.sessions.extend(mouse_sessions)
-            self.sessions_by_mouse_id[mouse] = mouse_sessions
+    def process_lfp(self, resample=True):
+        """
+        Process the LFP data from the raw neural recording data for all sessions.
+        """
+        for session in self.sessions:
+            session.process_lfp(resample=resample)
 
     def process_behaviour(self):
+        """
+        Process behavioural data from raw tdms files for all sessions.
+        """
         for session in self.sessions:
-            session.create_action_labels()
+            session.process_behaviour()
+
+    def process_motion_tracking(self):
+        """
+        Process motion tracking data either from raw camera data, or from
+        previously-generated deeplabcut coordinate data, for all sessions.
+        """
+        for session in self.sessions:
+            session.process_motion_tracking()
--- pixels/session.py
+++ pixels/session.py
@@ -1,8 +1,75 @@
+"""
+This module manipulates files and analyses at the session level.
+"""
+
+
+import datetime
+import json
+import os
+
+from pixels import ioutils
+
+
+_SAMPLE_RATE = 1000
 
 
 class Session:
     def __init__(self, metadata=None):
         self.metadata = metadata
 
-    def create_action_labels(self):
-        # do whatever
+    def extract_spikes(self, resample=True):
+        """
+        Extract the spikes from raw spike data.
+        """
+
+    def process_lfp(self):
+        """
+        Process the LFP data from the raw neural recording data.
+        """
+
+    def process_behaviour(self):
+        """
+        Process behavioural data from raw tdms files.
+        """
+
+    def process_motion_tracking(self):
+        """
+        Process motion tracking data either from raw camera data, or from
+        previously-generated deeplabcut coordinate data.
+        """
+
+
+def list_sessions(mouse_ids, data_dir, meta_dir):
+    """
+    Get a list of recording sessions for the specified mice, excluding those whose
+    metadata contain '"exclude" = True'.
+    """
+    meta_dir = os.path.expanduser(meta_dir)
+    available_sessions = os.listdir(os.path.expanduser(data_dir))
+    sessions = []
+
+    for mouse in mouse_ids:
+        mouse_sessions = []
+        for session in available_sessions:
+            if mouse in session:
+                mouse_sessions.append(session)
+
+        if mouse_sessions:
+            with open(os.path.join(meta_dir, mouse + '.json'), 'r') as fd:
+                mouse_meta = json.load(fd)
+            session_dates = [
+                datetime.datetime.strptime(s[0:6], '%y%m%d') for s in mouse_sessions
+            ]
+            for session in mouse_meta:
+                meta_date = datetime.datetime.strptime(session['date'], '%Y-%m-%d')
+                for index, ses_date in enumerate(session_dates):
+                    if ses_date == meta_date:
+                        if session.get('exclude', False):
+                            continue
+                        mouse_sessions[index] = Session(metadata=session)
+        else:
+            print(f'Found no sessions for: {mouse}')
+
+        sessions.extend(mouse_sessions)
+
+    return sessions
