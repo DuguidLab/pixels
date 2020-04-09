943c918855d82f4da972966577deedf747b170ae
mcol <mcol@posteo.net>
2020-04-09 16:17:48 +0100

    add handler to load data from BIN files

--- pixels/ioutils.py
+++ pixels/ioutils.py
@@ -5,55 +5,10 @@ This module contains helper functions for reading and writing files.
 
 import glob
 import os
+import numpy as np
+import pandas as pd
 from pathlib import Path
 from nptdms import TdmsFile
-
-
-def read_tdms(path, groups=None):
-    """
-    Read data from a TDMS file.
-
-    Parameters
-    ----------
-    path : str
-        Path to the TDMS file to be read.
-
-    groups : list of strs (optional)
-        Names of groups stored inside the TDMS file that should be loaded. By default,
-        all groups are loaded, so specifying the groups you want explicitly can avoid
-        loading the entire file from disk.
-
-    """
-    with TdmsFile.open(path) as tdms_file:
-        if groups is None:
-            df = tdms_file.as_dataframe()
-        else:
-            data = []
-            for group in groups:
-                channel = tdms_file[group].channels()[0]
-                group_data = tdms_file[group].as_dataframe()
-                group_data = group_data.rename(columns={channel.name:channel.path})
-                data.append(group_data)
-            df = pd.concat(data, axis=1)
-    return df
-
-
-def read_meta(path):
-    """
-    Read metadata from a .meta file file.
-
-    Parameters
-    ----------
-    path : str
-        Path to the meta file to be read.
-
-    """
-    metadata = {}
-    for entry in Path(path).read_text().split("\n"):
-        if entry:
-            key, value = entry.split("=")
-            metadata[key] = value
-    return metadata
 
 
 def get_data_files(data_dir, session_name):
@@ -107,9 +62,84 @@ def get_data_files(data_dir, session_name):
         if len(behaviour) == len(spike_data):
             recording['behaviour'] = behaviour[num]
         else:
-            recording['behaviour'] = behaviour
+            recording['behaviour'] = behaviour[0]
         recording['camera_data'] = camera_data[num]
         recording['camera_meta'] = camera_meta[num]
         files.append(recording)
 
     return files
+
+
+def read_meta(path):
+    """
+    Read metadata from a .meta file.
+
+    Parameters
+    ----------
+    path : str
+        Path to the meta file to be read.
+
+    """
+    metadata = {}
+    for entry in Path(path).read_text().split("\n"):
+        if entry:
+            key, value = entry.split("=")
+            metadata[key] = value
+    return metadata
+
+
+def read_bin(path, num_chans, channel=None):
+    """
+    Read data from a bin file.
+
+    Parameters
+    ----------
+    path : str
+        Path to the bin file to be read.
+
+    num_chans : int
+        The number of channels of data present in the file.
+
+    channel : int, optional
+        The channel to read. If None (default), all channels are read.
+
+    """
+    if not isinstance(num_chans, int):
+        num_chans = int(num_chans)
+
+    mapping = np.memmap(path, np.int16, mode='r').reshape((-1, num_chans))
+
+    if channel is not None:
+        mapping = mapping[:, channel]
+
+    return pd.DataFrame(data=mapping)
+
+
+def read_tdms(path, groups=None):
+    """
+    Read data from a TDMS file.
+
+    Parameters
+    ----------
+    path : str
+        Path to the TDMS file to be read.
+
+    groups : list of strs (optional)
+        Names of groups stored inside the TDMS file that should be loaded. By default,
+        all groups are loaded, so specifying the groups you want explicitly can avoid
+        loading the entire file from disk.
+
+    """
+    with TdmsFile.read(path) as tdms_file:
+        if groups is None:
+            #groups = [group.name for group in tdms_file.groups()]
+            df = tdms_file.as_dataframe()
+        else:
+            data = []
+            for group in groups:
+                channel = tdms_file[group].channels()[0]
+                group_data = tdms_file[group].as_dataframe()
+                group_data = group_data.rename(columns={channel.name:channel.path})
+                data.append(group_data)
+            df = pd.concat(data, axis=1)
+    return df
